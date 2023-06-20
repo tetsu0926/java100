e9c83e463c7d1ae97af616504fd04812397a68f3
totoro <tetsu0926@gmail.com>
2023-06-20 22:06:30 +0900

    dev

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,7 @@ public class Main {
 
         System.out.println("Hello world!");
 
-        System.out.println("feature");
-
-
-        System.out.println("100");
+        System.out.println("dev");
 
     }
 }
