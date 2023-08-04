2e80e6c7f8825283a9072f15f1c7a6e0460ba29b
totoro <tetsu0926@gmail.com>
2023-08-05 08:44:52 +0900

    git test commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,33 @@
 package org.example;
 
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
 
-        System.out.println("f200");
+        Map<String, Object> map = new HashMap<>();
+        map.put("test", "test1");
+
+
+        Calendar cal = Calendar.getInstance();
+        long start = cal.getTimeInMillis();
+
+        for(int i=0; i<10000000; i++){
+            String name = Optional.ofNullable(map.get("name")).orElseGet(()->"").toString();
+            System.out.println("name :"+ name);
+        }
+
+        cal = Calendar.getInstance();
+        long end = cal.getTimeInMillis();
+
+        System.out.println(end - start);
+
+        System.out.println("git test..");
+
 
     }
 }
